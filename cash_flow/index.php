1da1b8cb70b7269a4a5d0bc26c393bd2c4af22f1
Ziaagsttaa <iagustinzahra@gmail.com>
2024-06-30 00:52:24 +0700

    GANTI NAMA ISI TAHAP 1
    ganti anggota menjadi mahasiswa
    
    Co-Authored-By: alippzz <173701587+alippzz@users.noreply.github.com>
    Co-Authored-By: TheFREeze01 <168277694+TheFREeze01@users.noreply.github.com>

--- cash_flow/index.php
+++ cash_flow/index.php
@@ -253,7 +253,7 @@ include 'config/koneksi.php';
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Jumlah Anggota</div>
+                      <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Jumlah Mahasiswa</div>
                       <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $jum_anggota." Orang";?></div>
                     </div>
                     <div class="col-auto">
